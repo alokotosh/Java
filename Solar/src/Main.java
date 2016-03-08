0737ed983eec04e1bf0b2c7cafada3fe4145cac9
Andriy Lokotosh <andriy.lokotosh@gmail.com>
2016-03-08 14:08:01 +0200

    Solar project

---
+++ Solar/src/Main.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+import java.awt.*;
+
+/*ssss
+ * Created by kepler23 on 8/26/15.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+        {
+            Solar frame = new Solar();
+            frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+            frame.setVisible(true);
+
+
+        }}}
---
+++ Solar/src/Solar.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by kepler23 on 8/26/15.
+ */
+public class Solar extends  JFrame {
+    private JButton SendB;
+    private JTextArea textArea1;
+    private JTextField SendTextField;
+    private JFormattedTextField formattedTextField1;
+
+    private void createUIComponents() {
+
+
+
+        // TODO: place custom component creation code here
+    }
+}
